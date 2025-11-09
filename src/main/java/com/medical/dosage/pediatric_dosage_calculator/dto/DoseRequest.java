7341086b63fd88ba9d68728af39b8c0a3bb90301
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2025-11-09 16:50:20 -0500

    dto: DoseRequest edad/age en meses y años

--- src/main/java/com/medical/dosage/pediatric_dosage_calculator/dto/DoseRequest.java
+++ src/main/java/com/medical/dosage/pediatric_dosage_calculator/dto/DoseRequest.java
@@ -3,6 +3,8 @@ package com.medical.dosage.pediatric_dosage_calculator.dto;
 public class DoseRequest {
     private String medicineName;
     private double weightkg;
+    private int ageMonths;
+    private int ageYears;
 
     //Getters y swetter
     public String getMedicineName() {return medicineName;}
@@ -11,4 +13,9 @@ public class DoseRequest {
     public double getWeightKg() {return weightkg;}
     public void setWeightKg(double weightKg) {this.weightkg = weightKg;}
 
+    public int getAgeMonths() { return ageMonths; }
+    public void setAgeMonths(int ageMonths) { this.ageMonths = ageMonths;}
+
+     public int getAgeYears() { return ageYears; }
+    public void setAgeYears(int ageYears) { this.ageYears = ageYears;}
 }
